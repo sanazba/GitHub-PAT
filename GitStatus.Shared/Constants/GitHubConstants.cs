0cfc9493240755843b87b901720851926d816bdd
sanaz baniasadi <s.baniasadi@asax.local>
2021-07-12 16:52:43 +0430

    Initial comit

---
+++ GitStatus.Shared/Constants/GitHubConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace GitStatus.Shared
+{
+    static class GitHubConstants
+    {
+        public const string AuthScheme = "bearer";
+        public const string PersonalAccessToken = "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";
+        public const string GitHubRestApiUrl = "https://api.github.com";
+        public const string GitHubGraphQLApiUrl = "https://api.github.com/graphql";
+    }
+}
